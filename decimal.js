bda27d1ced813c4a67537de9845c4bdac78f4540
x1n9f2 <david.fu.df@gmail.com>
2017-05-26 12:25:42 -0400

    deleted decimal file

--- decimal.js
+++
@@ -1,9 +0,0 @@
-var exports = module.exports = {};
-
-exports.isNearZero = function(number, eps=1e-10){
-	return Math.abs(number) < eps;
-}
-
-// let d = new Decimal();
-// console.log(d.is_near_zero(0.00000000000000000004));
-
